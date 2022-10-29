8ab0249b5479e8da24e9eaa97188a523c8205511
TungTLT <tranlethanhtung554@gmail.com>
2022-10-29 11:24:16 +0700

    load prediction model when model already exist

--- chatbot_conversation_model/chatbot.py
+++ chatbot_conversation_model/chatbot.py
@@ -1,21 +1,18 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
-
-lemmatizer = WordNetLemmatizer()
+import json
+import random
 import pickle
 import numpy as np
 from keras.models import load_model
 from pathlib import Path
 
-
 projectLocation = Path(__file__).absolute().parent
 model = load_model(f'{projectLocation}/chatbot_model.h5')
-import json
-import random
-
 intents = json.loads(open(f'{projectLocation}/intents.json').read())
 words = pickle.load(open(f'{projectLocation}/words.pkl', 'rb'))
 classes = pickle.load(open(f'{projectLocation}/classes.pkl', 'rb'))
+lemmatizer = WordNetLemmatizer()
 
 
 def clean_up_sentence(sentence):
@@ -40,7 +37,7 @@ def bow(sentence, words, show_details=True):
                 bag[i] = 1
                 if show_details:
                     print("found in bag: %s" % w)
-    return (np.array(bag))
+    return np.array(bag)
 
 
 def predict_class(sentence):
@@ -57,7 +54,7 @@ def predict_class(sentence):
     return return_list
 
 
-def getResponse(ints, intents_json):
+def get_response(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
@@ -69,58 +66,58 @@ def getResponse(ints, intents_json):
 
 def chatbot_response(msg):
     ints = predict_class(msg)
-    res = getResponse(ints, intents)
+    res = get_response(ints, intents)
     return res
 
+# ============================= GUI =========================================
 # Creating GUI with tkinter
-import tkinter
-from tkinter import *
-
-
-def send():
-    msg = EntryBox.get("1.0", 'end-1c').strip()
-    EntryBox.delete("0.0", END)
-
-    if msg != '':
-        ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12))
-
-        res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
-
-        ChatLog.config(state=DISABLED)
-        ChatLog.yview(END)
-
-
-base = Tk()
-base.title("Hello")
-base.geometry("400x500")
-base.resizable(width=FALSE, height=FALSE)
-
-# Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial", )
-
-ChatLog.config(state=DISABLED)
-
-# Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
-
-# Create Button to send message
-SendButton = Button(base, font=("Verdana", 12, 'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff',
-                    command=send)
-
-# Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
-# EntryBox.bind("<Return>", send)
-
-
-# Place all components on the screen
-scrollbar.place(x=376, y=6, height=386)
-ChatLog.place(x=6, y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
-
-base.mainloop()
+# import tkinter
+# from tkinter import *
+#
+#
+# def send():
+#     msg = EntryBox.get("1.0", 'end-1c').strip()
+#     EntryBox.delete("0.0", END)
+#
+#     if msg != '':
+#         ChatLog.config(state=NORMAL)
+#         ChatLog.insert(END, "You: " + msg + '\n\n')
+#         ChatLog.config(foreground="#442265", font=("Verdana", 12))
+#
+#         res = chatbot_response(msg)
+#         ChatLog.insert(END, "Bot: " + res + '\n\n')
+#
+#         ChatLog.config(state=DISABLED)
+#         ChatLog.yview(END)
+#
+# base = Tk()
+# base.title("Hello")
+# base.geometry("400x500")
+# base.resizable(width=FALSE, height=FALSE)
+#
+# # Create Chat window
+# ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial", )
+#
+# ChatLog.config(state=DISABLED)
+#
+# # Bind scrollbar to Chat window
+# scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+# ChatLog['yscrollcommand'] = scrollbar.set
+#
+# # Create Button to send message
+# SendButton = Button(base, font=("Verdana", 12, 'bold'), text="Send", width="12", height=5,
+#                     bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff',
+#                     command=send)
+#
+# # Create the box to enter message
+# EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
+# # EntryBox.bind("<Return>", send)
+#
+#
+# # Place all components on the screen
+# scrollbar.place(x=376, y=6, height=386)
+# ChatLog.place(x=6, y=6, height=386, width=370)
+# EntryBox.place(x=128, y=401, height=90, width=265)
+# SendButton.place(x=6, y=401, height=90)
+#
+# base.mainloop()
--- chatbotdisisease_prediction_model_v2/chatbot_disease_prediction_v2.py
+++ chatbotdisisease_prediction_model_v2/chatbot_disease_prediction_v2.py
@@ -7,6 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
+import pickle
+from pathlib import Path
+
 
 # Reading the train.csv by removing the
 # last column since it's an empty column
@@ -32,11 +35,36 @@ models = {
 
 # Training the models on whole data
 final_svm_model = SVC()
-final_nb_model = GaussianNB()
+final_gnb_model = GaussianNB()
 final_rf_model = RandomForestClassifier(random_state=18)
-final_svm_model.fit(X, y)
-final_nb_model.fit(X, y)
-final_rf_model.fit(X, y)
+
+
+projectLocation = Path(__file__).absolute().parent
+rf_model_file = 'rf_model.pkl'
+gnb_model_file = 'gnb_model.pkl'
+svm_model_file = 'svm_model.pkl'
+
+rf_model_path = Path(f'{projectLocation}/prediction_model/{rf_model_file}')
+gnb_model_path = Path(f'{projectLocation}/prediction_model/{gnb_model_file}')
+svm_model_path = Path(f'{projectLocation}/prediction_model/{svm_model_file}')
+
+if rf_model_path.exists():
+    final_rf_model = pickle.load(open(rf_model_path, 'rb'))
+else:
+    final_rf_model.fit(X, y)
+    pickle.dump(final_rf_model, open(rf_model_path, 'wb'))
+
+if gnb_model_path.exists():
+    final_gnb_model = pickle.load(open(gnb_model_path, 'rb'))
+else:
+    final_gnb_model.fit(X, y)
+    pickle.dump(final_gnb_model, open(gnb_model_path, 'wb'))
+
+if svm_model_path.exists():
+    final_svm_model = pickle.load(open(svm_model_path, 'rb'))
+else:
+    final_svm_model.fit(X, y)
+    pickle.dump(final_svm_model, open(svm_model_path, 'wb'))
 
 symptoms = X.columns.values
 
@@ -64,7 +92,7 @@ def predict_disease(input_symptom):
     for symptom in input_symptom:
         symptom = symptom.lower()
         if symptom in data_dict['symptom_index']:
-            index = data_dict["symptom_index"][symptom.lower()]
+            index = data_dict["symptom_index"][symptom]
             input_data[index] = 1
         else:
             return "NO DATA"
@@ -75,7 +103,7 @@ def predict_disease(input_symptom):
 
     # generating individual outputs
     rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
-    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
+    nb_prediction = data_dict["predictions_classes"][final_gnb_model.predict(input_data)[0]]
     svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
 
     # making final prediction by taking mode of all predictions
@@ -91,4 +119,5 @@ def predict_disease(input_symptom):
 
 
 # Testing the function
-print(predict_disease("shivering"))
+user_input = input("Enter disease sympton: ")
+print(predict_disease(user_input))
